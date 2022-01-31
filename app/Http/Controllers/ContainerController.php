d9ac2ad1d8c271a8c14002ce62c4c0d0b5ce40bf
Yve Jose Zulatto <38947675+zulatto@users.noreply.github.com>
2022-01-31 18:56:51 -0300

    update controller

--- app/Http/Controllers/ContainerController.php
+++ app/Http/Controllers/ContainerController.php
@@ -4,15 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Brick\Math\Exception\IntegerOverflowException;
+use App\Models\Container;
 
 class ContainerController extends Controller
 {
-    public function Container(){
-        return view('container.index');
+    public function index()
+    {
+        return view('container.index', ['containers' => Container::all()]);
     }
 
-    // public function index()
-    // {
-    //     return view('container.index', ['containers' => Container::all()]);
-    // }
+    public function create(){
+        return view('container.create');
+    }
+
+    public function store(Request $request )
+    {
+        Container::create($request->all());
+
+        return redirect()->route('container.index')
+        ->with('success','Container inserido com sucesso.');
+    }
+
+
+    public function update(int $id)
+    {
+        return view('container.edit', ['container' => Container::findOrFail($id)]);
+    }
+
+    public function updateCntr(Request $request, int $id)
+    {
+        Container::findOrFail($id)->update($request->all());
+        
+        return redirect()->route('container.index');
+    }
+
+    public function destroy(int $id)
+    {
+        Container::destroy($id);
+        return redirect()->route('container.index');
+    }
 }
