fef3a1e12541ccdff5e3f2aff424642ee80173e7
Yve Jose Zulatto <38947675+zulatto@users.noreply.github.com>
2022-01-31 18:55:27 -0300

    update model

--- app/Models/Container.php
+++ app/Models/Container.php
@@ -2,14 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Container extends Model
 {
-    use HasFactory;
-
-    public $timestamps = false;
+    public $timestamps = true;
 
     protected $fillable = ['cliente', 'container', 'tipo', 'data'];
 
